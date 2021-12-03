1a0012d7d0fa7743ee179a86891b34b3b0eff971
throne.yash@gmail.com <throne.yash@gmail.com>
2021-12-02 21:25:11 -0700

    Adding Slider, Categories and Products to the main page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import Announcement from '../components/Announcement'
+import Categories from '../components/Categories'
 import Navbar from '../components/Navbar'
+import Products from '../components/Products'
+import Slider from '../components/Slider'
 
 const Home = () => {
   return (
     <div>
       <Announcement />
       <Navbar />
+      <Slider />
+      <Categories />
+      <Products />
     </div>
   )
 }
