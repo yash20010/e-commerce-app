17216c1e65a877efd64edf9aa3a1713cafa2de25
throne.yash@gmail.com <throne.yash@gmail.com>
2021-12-04 18:10:18 -0700

    Adding Product and ProductList to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Product from './pages/Product'
 import Home from './pages/Home'
+import ProductList from './pages/ProductList'
 
 const App = () => {
   return (
     <div>
-      <Home />
+      <Product />
     </div>
   )
 }
