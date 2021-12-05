b3d70f34ca95682bb28f79056a28919497aaa6bc
throne.yash@gmail.com <throne.yash@gmail.com>
2021-12-04 22:13:45 -0700

    Adding footer to the main page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,136 @@
+import { flexbox } from '@mui/system'
+import styled from 'styled-components'
+import FacebookIcon from '@mui/icons-material/Facebook'
+import InstagramIcon from '@mui/icons-material/Instagram'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import PinterestIcon from '@mui/icons-material/Pinterest'
+import RoomIcon from '@mui/icons-material/Room'
+import PhoneIcon from '@mui/icons-material/Phone'
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined'
+
+const Container = styled.div`
+  display: flex;
+`
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`
+
+const Logo = styled.h1``
+const Desc = styled.p`
+  margin: 20px 0;
+`
+const SocialContainer = styled.div`
+  display: flex;
+`
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 20px;
+`
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`
+const Title = styled.h3`
+  margin-bottom: 20px;
+`
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`
+
+const Right = styled.div`
+  flex: 1;
+
+  padding: 20px;
+`
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`
+
+const Payment = styled.img`
+  width: 50%;
+`
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>LAMA</Logo>
+        <Desc>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis,
+          minus cum cupiditate, delectus ipsa nihil veritatis ea excepturi
+          commodi voluptates doloremque assumenda iusto maxime accusantium
+          dolorem. Dignissimos, quia. Recusandae, natus?
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <FacebookIcon />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <InstagramIcon />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <TwitterIcon />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <PinterestIcon />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+
+        <ContactItem>
+          <RoomIcon style={{ marginRight: '10px' }} /> 622 Dixie Path , South
+          Tobinchester 98336
+        </ContactItem>
+        <ContactItem>
+          <PhoneIcon style={{ marginRight: '10px' }} /> +1 234 56 78
+        </ContactItem>
+        <ContactItem>
+          <EmailOutlinedIcon style={{ marginRight: '10px' }} /> contact@lama.dev
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
+    </Container>
+  )
+}
+
+export default Footer
