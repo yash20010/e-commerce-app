1c9708cabec95731f9f830eb1c4a8b42efef63ea
throne.yash@gmail.com <throne.yash@gmail.com>
2021-12-05 18:33:16 -0700

    Adding Register and Login to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Product from './pages/Product'
 import Home from './pages/Home'
 import ProductList from './pages/ProductList'
+import Register from './pages/Register'
+import Login from './pages/Login'
 
 const App = () => {
   return (
     <div>
-      <Product />
+      <Login />
     </div>
   )
 }
