7692c3326aff665a32d75db8a47c88c14db6a7d1
throne.yash@gmail.com <throne.yash@gmail.com>
2021-12-08 23:24:57 -0700

    Making the Navbar mobile friendly

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import styled from 'styled-components'
 import SearchIcon from '@mui/icons-material/Search'
 import Badge from '@mui/material/Badge'
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined'
+import { mobile } from '../responsive'
 
 const Container = styled.div`
   height: 60px;
+  ${mobile({ height: '50px' })}
 `
 
 const Wrapper = styled.div`
@@ -12,10 +14,12 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  ${mobile({ padding: '10px 0' })}
 `
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
+  ${mobile({ display: 'none' })}
 `
 
 const Left = styled.div`
@@ -34,6 +38,7 @@ const SearchContainer = styled.div`
 
 const Input = styled.input`
   border: none;
+  ${mobile({ width: '50px' })}
 `
 
 const Center = styled.div`
@@ -42,6 +47,7 @@ const Center = styled.div`
 `
 const Logo = styled.h1`
   font-weight: bold;
+  ${mobile({ fontSize: '24px' })}
 `
 
 const Right = styled.div`
@@ -49,12 +55,14 @@ const Right = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  ${mobile({ flex: '2', justifyContent: 'center' })}
 `
 
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
   margin-left: 25px;
+  ${mobile({ fontSize: '12px', marginLeft: '10px' })}
 `
 
 const Navbar = () => {
@@ -64,7 +72,7 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input />
+            <Input placeholder="Search" />
             <SearchIcon style={{ color: 'gray', fontSize: 16 }} />
           </SearchContainer>
         </Left>
