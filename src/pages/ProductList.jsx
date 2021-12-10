98a35c2b2eeacfb774f0cfc26116cbc985889298
throne.yash@gmail.com <throne.yash@gmail.com>
2021-12-09 23:31:29 -0700

    Making the ProductList page mobile friendly

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -4,6 +4,7 @@ import Announcements from '../components/Announcement'
 import Products from '../components/Products'
 import Newsletter from '../components/Newsletter'
 import Footer from '../components/Footer'
+import { mobile } from '../responsive'
 
 const Container = styled.div``
 const Title = styled.h1`
@@ -15,16 +16,19 @@ const FilterContainer = styled.div`
 `
 const Filter = styled.div`
   margin: 20px;
+  ${mobile({ margin: '0 20px', display: 'flex', flexDirection: 'column' })}
 `
 const FilterText = styled.span`
   font-size: 20px;
   font-weight: 600;
   margin-right: 20px;
+  ${mobile({ marginRight: '0' })}
 `
 
 const Select = styled.select`
   padding: 10px;
   margin-right: 20px;
+  ${mobile({ marginRight: '10px 0' })}
 `
 const Option = styled.option``
 
