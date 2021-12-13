494699a17d22a20dfae37471997de1888958a302
throne.yash@gmail.com <throne.yash@gmail.com>
2021-12-13 13:11:59 -0700

    Adding Order model

---
+++ api/models/Order.js
@@ -0,0 +1,38 @@
+// Create Order model
+const mongoose = require('mongoose')
+
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    products: [
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+    amount: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: Object,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: 'pending',
+    },
+  },
+  { timestamps: true }
+)
+
+// Export Order model
+module.exports = mongoose.model('Order', OrderSchema)
