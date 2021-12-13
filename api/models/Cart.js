fa0e71e6a359f12d7dfc1790ecb1b423aa976110
throne.yash@gmail.com <throne.yash@gmail.com>
2021-12-13 13:11:32 -0700

    Adding cart model

---
+++ api/models/Cart.js
@@ -0,0 +1,26 @@
+// Create Cart model
+const mongoose = require('mongoose')
+
+const CartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    products: [
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+)
+
+// Export Cart model
+module.exports = mongoose.model('Cart', CartSchema)
