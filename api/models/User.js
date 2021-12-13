3842021184c321a121fe3fc404d55e26db467044
throne.yash@gmail.com <throne.yash@gmail.com>
2021-12-13 13:12:39 -0700

    Adding User model

---
+++ api/models/User.js
@@ -0,0 +1,29 @@
+// Create User model
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+)
+
+// Export User model
+module.exports = mongoose.model('User', userSchema)
