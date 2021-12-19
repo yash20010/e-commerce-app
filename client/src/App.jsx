5a2476a80624cbbdcfcc261d4aa3d1d0aec54ca6
throne.yash@gmail.com <throne.yash@gmail.com>
2021-12-19 14:07:17 -0700

    Adding home products and product routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,21 @@ import ProductList from './pages/ProductList'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Cart from './pages/Cart'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      <Cart />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/products" element={<ProductList />}>
+          <Route path=":category" element={<ProductList />}></Route>
+        </Route>
+        <Route path="/product" element={<Product />}>
+          <Route path=":id" element={<Product />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
