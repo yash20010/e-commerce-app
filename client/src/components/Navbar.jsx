6eb03cbd8119563b6b82808ed51c3df7fe72d3b2
throne.yash@gmail.com <throne.yash@gmail.com>
2021-12-23 18:31:09 -0700

    Adding link to the cart

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import Badge from '@mui/material/Badge'
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined'
 import { mobile } from '../responsive'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const Container = styled.div`
   height: 60px;
@@ -85,11 +86,13 @@ const Navbar = () => {
         <Right>
           <MenuItem>REGISTER</MenuItem>
           <MenuItem>SIGN IN</MenuItem>
-          <MenuItem>
-            <Badge badgeContent={quantity} color="primary">
-              <ShoppingCartOutlinedIcon />
-            </Badge>
-          </MenuItem>
+          <Link to="/cart">
+            <MenuItem>
+              <Badge badgeContent={quantity} color="primary">
+                <ShoppingCartOutlinedIcon />
+              </Badge>
+            </MenuItem>
+          </Link>
         </Right>
       </Wrapper>
     </Container>
